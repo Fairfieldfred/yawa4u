f14d1a3fbc6b1191d7a8924fa89cc548b4b80483
Fred Blair <fredblair@att.net>
2026-06-05 10:32:36 -0700

    feat: update AndroidManifest.xml to comment out unused health permissions and format intent-filter

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ if (localPropertiesFile.exists()) {
 }
 
 
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.2.0"
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 120
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.2.2"
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 122
 val flutterMinSdkVersion = localProperties.getProperty("flutter.minSdkVersion")?.toInt() ?: 26
 val flutterTargetSdkVersion = localProperties.getProperty("flutter.targetSdkVersion")?.toInt() ?: 35
 
